64e0c7fbe2a662ae95ae11f97d012d9ecbb0e244
Steven Hillcox <shillcox@scottlogic.co.uk>
2014-09-09 14:44:30 +0100

    Deployment now assumes a copy of mongodb is running and will no longer try to start new instances of it.

--- deploy.bat
+++
@@ -1,6 +0,0 @@
-:: Shutdown node and mongo
-taskkill /IM mongod.exe /F
-taskkill /IM node.exe /F
-:: Start node and mongo
-START /B C:\"Program Files"\"MongoDB 2.6 Standard"\bin\mongod.exe -dbpath Server/data/db/
-START /B node Server/main
