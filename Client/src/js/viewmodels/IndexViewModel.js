d15f9021e60f6396e7e06d5a168a2f273d6bc442
Tyler Ferguson <tferguson@scottlogic.com>
2014-09-11 13:51:55 +0000

    Fixed NaN bug in leaderboard when new users join lobby

--- Client/src/js/viewmodels/IndexViewModel.js
+++ Client/src/js/viewmodels/IndexViewModel.js
@@ -44,8 +44,14 @@ define(['jQuery', 'knockout', 'websocket/WebSocketClient', 'game/Game'], functio
                 type: 'GET',
                 success: function(data) {
                     data.sort( function(a, b) {
-                        var bPercent = 100 * b.wins / b.gamesPlayed;
-                        var aPercent = 100 * a.wins / a.gamesPlayed;
+                        var bPercent;
+                        var aPercent;
+                        
+                        bPercent = 100 * b.wins / b.gamesPlayed;
+                        aPercent = 100 * a.wins / a.gamesPlayed;
+                        
+                        aPercent = aPercent || 0;
+                        bPercent = bPercent || 0;
                         
                         if (aPercent === bPercent) {
                             return b.wins - a.wins;
